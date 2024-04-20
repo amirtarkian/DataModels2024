8e8d6050c06738b40bae2b98f0c030bedb22708d
Ryan Flatley <rflatley2@huskers.unl.edu>
2024-04-19 21:29:52 -0500

    more data

--- data/sentiment_analyzer.py
+++ data/sentiment_analyzer.py
@@ -52,9 +52,9 @@ def save_to_json(data, filename):
         json.dump(data, f, indent=4)
 
 ticker = 'AAPL'
-end_date =datetime(2023, 10, 27)
-# start_date = end_date - timedelta(days=86)
-start_date = datetime(2023,9,30)
+
+end_date = datetime(2023,9,30)
+start_date = end_date - timedelta(days=72)
 
 sentiment_data = {}
 last_valid_sentiment = 0.00
